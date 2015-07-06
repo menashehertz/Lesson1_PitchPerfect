116da2a8bb1b7f31d9edf0f6fa64ef4798025b30
menashehertz <steven.hertz@tech4everyone.com>
2015-07-06 12:03:31 -0400

    2: fixed and working player

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -16,53 +16,27 @@ class PlaySoundsViewController: UIViewController {
   
   @IBAction func slowPlay(sender: AnyObject) {
     println("PLay button pressed")
+    audioPlayer.prepareToPlay()
+    audioPlayer.play()
+    
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // var error = NSError()
 
-    let url = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")!)
+    let url = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("movie_quot", ofType: "mp3")!)
+    audioPlayer = AVAudioPlayer(contentsOfURL: url, error: &error)
+    if audioPlayer == nil {
+      if let err = error {
+        println("in error \(err.localizedDescription)")
+      }
+      println("out of error")
+    }
     
-    audioPlayer = AVAudioPlayer(contentsOfURL: url, error: nil)
-    audioPlayer.prepareToPlay()
-    audioPlayer.play()
-
-    
-    
-   // audioPlayer = AVAudioPlayer(contentsOfURL: url, error: nil)
-    
-    
-//      if let
-//    audioPlayer = AVAudioPlayer(contentsOfURL: url, error: &error)
-    
-//       if let err = error {
-//             println("audioPlayer error \(err.localizedDescription)")
-//        } else {
-//             audioPlayer?.delegate = self
-//             audioPlayer?.prepareToPlay()
-//          }
-//
-
-
   }
-  
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
